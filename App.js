7efba51fd92546b9edbaebd9c77fa07ac5875106
John <codebeast0420@gmail.com>
2023-07-13 22:25:07 -0700

    fix status bar

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as Font from 'expo-font';
 import Navigation from './Navigation';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { StatusBar } from 'expo-status-bar';
 // import './main.css';
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
 
   return (
     <Provider store={store}>
+      <StatusBar translucent backgroundColor="transparent" />
       <Navigation />
     </Provider>
   );
